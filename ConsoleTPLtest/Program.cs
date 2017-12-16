b04321ca1e488184e71b75a9d38643754a1952ea
Javier De Carli <jdecarli@users.noreply.github.com>
2017-12-15 23:58:02 -0500

    first commit

---
+++ ConsoleTPLtest/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleTPLtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FirstInResponse();
+            WaitAllResponseInParallel();
+
+            Console.WriteLine("\ndone...");
+            Console.ReadKey();
+        }
+
+        public static void FirstInResponse()
+        {
+            Console.WriteLine($"\nStarting {nameof(FirstInResponse)} - Timestamp: {DateTime.Now.ToString("mm.ss.fff")}\n");
+
+            var firstValue = Task.Factory.StartNew(() => { return GetFirstValue().Result; });
+            var secondValue = Task.Factory.StartNew(() => { return GetSecondValue().Result; });
+            var thirdValue = Task.Factory.StartNew(() => { return GetThirdValue().Result; });
+
+            Task<string>[] tasks = { firstValue, secondValue, thirdValue };
+            var indexOfFirstTaskReturning = Task.WaitAny(tasks);
+
+            Console.WriteLine($"the first one returning is {tasks[indexOfFirstTaskReturning].Result}");
+            Console.WriteLine($"Completing {nameof(FirstInResponse)} at: {DateTime.Now.ToString("mm.ss.fff")}");
+        }
+
+        public static void WaitAllResponseInParallel()
+        {
+            Console.WriteLine($"\nStarting {nameof(WaitAllResponseInParallel)} - Timestamp: {DateTime.Now.ToString("mm.ss.fff")}\n");
+
+            var firstValue = Task.Factory.StartNew(() => { return GetFirstValue().Result; });
+            var secondValue = Task.Factory.StartNew(() => { return GetSecondValue().Result; });
+            var thirdValue = Task.Factory.StartNew(() => { return GetThirdValue().Result; });
+
+            Task<string>[] tasks = { firstValue, secondValue, thirdValue };
+            Task.WaitAll(tasks);
+
+            foreach (var t in tasks)
+            {
+                Console.WriteLine($"returning method: {t.Result}");
+            }
+
+            Console.WriteLine($"\nCompleting {nameof(WaitAllResponseInParallel)} at: {DateTime.Now.ToString("mm.ss.fff")}");
+        }
+
+        public static async Task<string> GetFirstValue()
+        {
+            var delay = 1500;
+
+            await Task.Delay(delay);
+
+            return $"{nameof(GetFirstValue)} - delay: {delay} - Timestamp: {DateTime.Now.ToString("mm.ss.fff")}";
+        }
+
+        public static async Task<string> GetSecondValue()
+        {
+            var delay = 1000;
+
+            await Task.Delay(delay);
+
+            return $"{nameof(GetSecondValue)} - delay: {delay} - Timestamp: {DateTime.Now.ToString("mm.ss.fff")}";
+        }
+
+        public static async Task<string> GetThirdValue()
+        {
+            var delay = 500;
+
+            await Task.Delay(delay);
+
+            return $"{nameof(GetThirdValue)} - delay: {delay} - Timestamp: {DateTime.Now.ToString("mm.ss.fff")}";
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# SandboxNetTPL
-Sandob to play with TPL
+# Sandbox .NET Core TPL (task Parallel Library)
+Sandbox to play with TPL
+
+## Summary
+Basically the project runs an example with `Task.WaitAny` and another with `Task.WaitAll`
+
+## Pre-requisites
+- .NET Core 2.0
