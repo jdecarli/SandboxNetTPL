b415eb7a4d5dd627d47b34d6a0a13d0edb9c19fa
Javier De Carli <jdecarli@users.noreply.github.com>
2018-01-14 00:30:51 -0500

    adding examples with sync methods wrapped on async methods

--- ConsoleTPLtest/Program.cs
+++ ConsoleTPLtest/Program.cs
@@ -1,18 +1,30 @@
-﻿using System;
-using System.Threading.Tasks;
-
-namespace ConsoleTPLtest
+﻿namespace ConsoleTPLtest
 {
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
     class Program
     {
+        private const int FirstValueDelay = 1500;
+        private const int SecondValueDelay = 1000;
+        private const int ThirdValueDelay = 500;
+
         static void Main(string[] args)
         {
+            Console.WriteLine("--ASYNC methods--");
             FirstInResponse();
             WaitAllResponseInParallel();
+
+            Console.WriteLine("\n--SYNC methods--");
+            FirstInResponseWithSyncMethods();
+            WaitAllResponseInParallelWithSyncMethods();
 
             Console.WriteLine("\ndone...");
             Console.ReadKey();
         }
+
+        #region Caling Async methods in parallel
 
         public static void FirstInResponse()
         {
@@ -50,7 +62,7 @@ namespace ConsoleTPLtest
 
         public static async Task<string> GetFirstValue()
         {
-            var delay = 1500;
+            var delay = FirstValueDelay;
 
             await Task.Delay(delay);
 
@@ -59,7 +71,7 @@ namespace ConsoleTPLtest
 
         public static async Task<string> GetSecondValue()
         {
-            var delay = 1000;
+            var delay = SecondValueDelay;
 
             await Task.Delay(delay);
 
@@ -68,11 +80,82 @@ namespace ConsoleTPLtest
 
         public static async Task<string> GetThirdValue()
         {
-            var delay = 500;
+            var delay = ThirdValueDelay;
 
             await Task.Delay(delay);
 
             return $"{nameof(GetThirdValue)} - delay: {delay} - Timestamp: {DateTime.Now.ToString("mm.ss.fff")}";
         }
+
+        #endregion
+
+        #region Calling Sync methods in parallel
+
+        public static void FirstInResponseWithSyncMethods()
+        {
+            Console.WriteLine($"\nStarting {nameof(FirstInResponseWithSyncMethods)} - Timestamp: {DateTime.Now.ToString("mm.ss.fff")}\n");
+
+            string firstResult, secondResult, thirdResult;
+
+            var firstTask = Task.Run(() => firstResult = GetFirstValueSync());
+            var secondTask = Task.Run(() => secondResult = GetSecondValueSync());
+            var thirdTask = Task.Run(() => thirdResult = GetThirdValueSync());
+
+            Task<string>[] tasks = { firstTask, secondTask, thirdTask };
+            var indexOfFirstTaskReturning = Task.WaitAny(tasks);
+
+            Console.WriteLine($"the first one returning is {tasks[indexOfFirstTaskReturning].Result}");
+            Console.WriteLine($"Completing {nameof(FirstInResponseWithSyncMethods)} at: {DateTime.Now.ToString("mm.ss.fff")}");
+        }
+
+        public static void WaitAllResponseInParallelWithSyncMethods()
+        {
+            Console.WriteLine($"\nStarting {nameof(WaitAllResponseInParallelWithSyncMethods)} - Timestamp: {DateTime.Now.ToString("mm.ss.fff")}\n");
+
+            string firstResult, secondResult, thirdResult;
+
+            var firstTask = Task.Run(() => firstResult = GetFirstValueSync());
+            var secondTask = Task.Run(() => secondResult = GetSecondValueSync());
+            var thirdTask = Task.Run(() => thirdResult = GetThirdValueSync());
+
+            Task<string>[] tasks = { firstTask, secondTask, thirdTask };
+            Task.WaitAll(tasks);
+
+            foreach (var t in tasks)
+            {
+                Console.WriteLine($"returning method: {t.Result}");
+            }
+
+            Console.WriteLine($"\nCompleting {nameof(WaitAllResponseInParallelWithSyncMethods)} at: {DateTime.Now.ToString("mm.ss.fff")}");
+        }
+
+        private static string GetFirstValueSync()
+        {
+            var delay = FirstValueDelay;
+
+            Thread.Sleep(delay);
+
+            return $"{nameof(GetFirstValueSync)} - delay: {delay} - Timestamp: {DateTime.Now.ToString("mm.ss.fff")}";
+        }
+
+        private static string GetSecondValueSync()
+        {
+            var delay = SecondValueDelay;
+
+            Thread.Sleep(delay);
+
+            return $"{nameof(GetSecondValueSync)} - delay: {delay} - Timestamp: {DateTime.Now.ToString("mm.ss.fff")}";
+        }
+
+        private static string GetThirdValueSync()
+        {
+            var delay = ThirdValueDelay;
+
+            Thread.Sleep(delay);
+
+            return $"{nameof(GetThirdValueSync)} - delay: {delay} - Timestamp: {DateTime.Now.ToString("mm.ss.fff")}";
+        }
+
+        #endregion
     }
 }
